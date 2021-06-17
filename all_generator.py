e2887fbac7e6b6b4e753fd18eb399c24adfb148f
Tony Hirst <tony.hirst@gmail.com>
2021-06-17 18:25:13 +0100

    Refactor; add subdir example

--- all_generator.py
+++ all_generator.py
@@ -16,7 +16,9 @@ class DateTimeEncoder(json.JSONEncoder):
 
         
 path = '.'
-outdir = Path(path)
+
+outdir = Path(path) / 'file_manifests'
+outdir.mkdir(exist_ok=True)
 
 files_dir = Path(path) / 'content'
 
@@ -30,5 +32,18 @@ all_json.write_text(
         json.dumps(
             fm.get(''), indent=2, sort_keys=True, cls=DateTimeEncoder
         ),
-        encoding="utf-8",
+        encoding="utf-8"
     )
+
+# We then need to do this for every directory in the contents dir
+for p in files_dir.iterdir():
+    if p.is_dir():
+        rel_p =  p.relative_to(str(files_dir))
+        
+        all_json = (outdir / rel_p / "all.json")
+        all_json.parent.mkdir(parents=True, exist_ok=True)
+        
+        all_json.write_text(
+            json.dumps(fm.get(str(rel_p)), indent=2, sort_keys=True, cls=DateTimeEncoder),
+            encoding="utf-8"
+        )
